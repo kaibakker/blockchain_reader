3181d9099d9fabc8c8d7592a1ebc45a59785cbd2
Kai Bakker <kaibakker@gmail.com>
2014-12-31 14:32:38 +0100

    little refactoring

--- block.rb
+++ block.rb
@@ -3,7 +3,7 @@ require_relative 'blocktools'
 
 
 class Block
-  attr_accessor :magic_number, :blocksize, :version, :previous_hash, :merkle_hash, :time, :bits, :nonce, :transactions_count, :transactions
+  attr_accessor :magic_number, :blocksize, :version, :previous_hash, :merkle_hash, :time, :bits, :nonce, :transactions
 
   def initialize(blockchain)
     @magic_number = uint4(blockchain)
@@ -15,10 +15,9 @@ class Block
     @bits = uint4(blockchain)
     @nonce = uint4(blockchain)
 
-    @transactions_count = varint(blockchain)
-    @transactions = []
-    @transactions_count.times do
-      @transactions << Transaction.new(blockchain)
+    transactions_count = varint(blockchain)
+    @transactions = Array.new(transactions_count) do
+      Transaction.new(blockchain)
     end
   end
 
@@ -40,21 +39,21 @@ class Block
 end
 
 class Transaction
-  attr_accessor :version, :inputs_count, :inputs, :outputs_count, :outputs, :lock_time
+  attr_accessor :version, :inputs, :outputs, :lock_time
+
   def initialize(blockchain)
     @version = uint4(blockchain)
 
-    @inputs_count = varint(blockchain)
-    @inputs = []
-    @inputs_count.times do
-      @inputs << TransactionInput.new(blockchain)
+    inputs_count = varint(blockchain)
+    @inputs = Array.new(inputs_count) do
+      TransactionInput.new(blockchain)
     end
-
-    @outputs_count = varint(blockchain)
-    @outputs = []
-    @outputs_count.times do
-      @outputs << TransactionOutput.new(blockchain)
+    
+    outputs_count = varint(blockchain)
+    @outputs = Array.new(outputs_count) do
+      TransactionOutput.new(blockchain)
     end
+    
     @lock_time = uint4(blockchain)
   end
 
@@ -74,20 +73,20 @@ class Transaction
 end
 
 class TransactionInput
-  attr_accessor :previous_hash, :transaction_output_id, :script_length, :script_sign, :sequal_number
+  attr_accessor :previous_hash, :transaction_output_id, :script_sign, :sequal_number
 
   def initialize(blockchain)
     @previous_hash = hash32(blockchain)
     @transactions_out_id = uint4(blockchain)
-    @script_length = varint(blockchain)
-    @script_sign = hash_with_length(blockchain, @script_length)
+
+    script_length = varint(blockchain)
+    @script_sign = hash_with_length(blockchain, script_length)
     @sequal_number = uint4(blockchain)
   end
 
   def to_s
-    string  = "Previous hash:\t#{@previous_hash}\n"
+    string  = "Previous Hash:\t#{@previous_hash}\n"
     string += "Tx Out Index:\t#{@transactions_out_id.to_s(16)}\n"
-    string += "Script Length:\t#{@script_length}\n"
     string += "Script Sign:\t#{@script_sign}\n"
     string += "Sequence:\t#{@sequal_number.to_s(16)}\n\n"
     string
@@ -100,13 +99,13 @@ class TransactionOutput
 
   def initialize(blockchain)
     @value = uint8(blockchain)
-    @script_length = varint(blockchain)
-    @public_key = hash_with_length(blockchain, @script_length)
+    script_length = varint(blockchain)
+    @public_key = hash_with_length(blockchain, script_length)
   end
 
   def to_s
     string  = "Value:\t\t#{@value}\n"
-    string += "Script Length:\t#{@script_length}\n"
+    
     string += "Public Key:\t#{@public_key}\n\n"
     string
   end
